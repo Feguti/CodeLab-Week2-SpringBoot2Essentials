f64074921a4475225d1a6baf035c53b47d19938d
Felipe Gutierrez <feguti4@gmail.com>
2022-05-20 00:07:59 -0300

    Implementing Post method

--- src/main/java/springboot/feguti/springboot2/service/AnimeService.java
+++ src/main/java/springboot/feguti/springboot2/service/AnimeService.java
@@ -5,12 +5,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import springboot.feguti.springboot2.domain.Anime;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-    private List<Anime> animes = List.of(new Anime(1l, "Kimetsu no Yaiba"), new Anime(2l, "FMA Brotherhood"), new Anime(3l, "Horimiya"));
+    private static List<Anime> animes;
 
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1l, "Kimetsu no Yaiba"), new Anime(2l, "FMA Brotherhood"), new Anime(3l, "Horimiya")));
+    }
     public List<Anime> listAll() {
         return animes;
     }
@@ -20,6 +25,12 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(4, 1000000));
+        animes.add(anime);
+        return anime;
     }
 }
 
