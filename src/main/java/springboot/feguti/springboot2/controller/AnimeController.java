d54ee6953d323ed751c35c028bb1b6d3c31b3120
Felipe Gutierrez <feguti4@gmail.com>
2022-05-19 23:35:30 -0300

    Uptading Get method to also return animes by id

--- src/main/java/springboot/feguti/springboot2/controller/AnimeController.java
+++ src/main/java/springboot/feguti/springboot2/controller/AnimeController.java
@@ -2,7 +2,10 @@ package springboot.feguti.springboot2.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springboot.feguti.springboot2.domain.Anime;
@@ -16,13 +19,19 @@ import java.util.List;
 @RequestMapping("animes")
 @Log4j2
 @RequiredArgsConstructor
+
 public class AnimeController {
     private final DateUtil dateUtil;
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return AnimeService.listAll();
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- src/main/java/springboot/feguti/springboot2/service/AnimeService.java
+++ src/main/java/springboot/feguti/springboot2/service/AnimeService.java
@@ -1,13 +1,25 @@
 package springboot.feguti.springboot2.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import springboot.feguti.springboot2.domain.Anime;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
-    public static List<Anime> listAll(){
-        return List.of(new Anime(1, "Kimetsu no Yaiba"), new Anime(2, "FMA Brotherhood"));
+    private List<Anime> animes = List.of(new Anime(1l, "Kimetsu no Yaiba"), new Anime(2l, "FMA Brotherhood"), new Anime(3l, "Horimiya"));
+
+    public List<Anime> listAll() {
+        return animes;
+    }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
+
