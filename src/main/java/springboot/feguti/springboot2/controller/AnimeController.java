d1a00c48ca639f9ed3f5849eea95df726de7a7ce
Felipe Gutierrez <feguti4@gmail.com>
2022-05-19 22:17:54 -0300

    First commit

---
+++ src/main/java/springboot/feguti/springboot2/controller/AnimeController.java
@@ -0,0 +1,26 @@
+package springboot.feguti.springboot2.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springboot.feguti.springboot2.domain.Anime;
+import springboot.feguti.springboot2.util.DateUtil;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+    private final DateUtil dateUtil;
+
+    @GetMapping(path = "list")
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Kimetsu no Yaiba"), new Anime("FMA Brotherhood"));
+    }
+}
