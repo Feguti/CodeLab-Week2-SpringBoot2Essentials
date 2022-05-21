1c0177628349bc7fb990503071f5e43314f98d4e
Felipe Gutierrez <feguti4@gmail.com>
2022-05-21 11:41:03 -0300

    Creating the anime database and adding new dependencies to run the database

--- src/main/java/springboot/feguti/springboot2/repository/AnimeRepository.java
+++ src/main/java/springboot/feguti/springboot2/repository/AnimeRepository.java
@@ -1,8 +1,10 @@
 package springboot.feguti.springboot2.repository;
 
+import org.springframework.stereotype.Repository;
 import springboot.feguti.springboot2.domain.Anime;
 import java.util.List;
 
+@Repository
 public interface AnimeRepository {
     List<Anime> listAll();
 
