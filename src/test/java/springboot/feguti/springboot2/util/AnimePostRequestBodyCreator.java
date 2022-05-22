58f97074cb716db0138c3bac7149ce981b3cb766
Felipe Gutierrez <feguti4@gmail.com>
2022-05-22 14:47:31 -0300

    Unit tests: listAll, find by id, find by name, save, replace and delete tests added

---
+++ src/test/java/springboot/feguti/springboot2/util/AnimePostRequestBodyCreator.java
@@ -0,0 +1,11 @@
+package springboot.feguti.springboot2.util;
+
+import springboot.feguti.springboot2.requests.AnimePostRequestBody;
+
+public class AnimePostRequestBodyCreator {
+    public static AnimePostRequestBody createAnimePostRequestBody(){
+        return AnimePostRequestBody.builder()
+                .name(AnimeCreator.createAnimeToBeSaved().getName())
+                .build();
+    }
+}
---
+++ src/test/java/springboot/feguti/springboot2/util/AnimePutRequestBodyCreator.java
@@ -0,0 +1,12 @@
+package springboot.feguti.springboot2.util;
+
+import springboot.feguti.springboot2.requests.AnimePutRequestBody;
+
+public class AnimePutRequestBodyCreator {
+    public static AnimePutRequestBody createAnimePutRequestBody(){
+        return AnimePutRequestBody.builder()
+                .id(AnimeCreator.createValidUpdatedAnime().getId())
+                .name(AnimeCreator.createValidUpdatedAnime().getName())
+                .build();
+    }
+}
